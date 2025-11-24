17e9967840fbea1773a085999f550b408868d627
SSSSSADO <grushkovik.danil@gmail.com>
2025-11-24 12:44:26 +0300

    fix

--- app/main.py
+++ app/main.py
@@ -1,19 +1,20 @@
+from __future__ import annotations
 from abc import ABC, abstractmethod
 
 
 class Validator(ABC):
-    def __set_name__(self, owner, name):
+    def __set_name__(self, owner: type, name: str) -> None:
         self.protected_name = "_" +name
 
-    def __get__(self, instance, owner):
+    def __get__(self, instance: object, owner: type) -> object:
         return getattr(instance, self.protected_name)
 
-    def __set__(self, instance, value):
+    def __set__(self, instance: object, value: object) -> None:
         self.validate(value)
         setattr(instance, self.protected_name, value)
 
     @abstractmethod
-    def validate(self, value):
+    def validate(self, value: object) -> None:
         pass
 
 
@@ -22,7 +23,7 @@ class Number(Validator):
         self.min_value = min_value
         self.max_value = max_value
 
-    def validate(self, value) -> None:
+    def validate(self, value: int) -> None:
         if not isinstance(value, int):
             raise TypeError("Quantity should be integer.")
         if not (self.min_value <= value <= self.max_value):
@@ -36,7 +37,7 @@ class OneOf(Validator):
     def __init__(self, options: tuple) -> None:
         self.options = options
 
-    def validate(self, value) -> None:
+    def validate(self, value: str) -> None:
         if value not in self.options:
             raise ValueError(f"Expected {value} to be one of {self.options}.")
 
