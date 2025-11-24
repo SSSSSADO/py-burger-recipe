e20be44ccd1f1ed791c44261e58b35cb7918e339
SSSSSADO <grushkovik.danil@gmail.com>
2025-11-24 12:05:32 +0300

    process

--- app/main.py
+++ app/main.py
@@ -11,4 +11,17 @@ class OneOf:
 
 
 class BurgerRecipe:
-    pass
+    def __init__(self,
+                 buns: int,
+                 cheese: int,
+                 tomatoes: int,
+                 cutlets: int,
+                 eggs: int,
+                 sauce: str
+                 ) -> None:
+        self.buns = buns
+        self.cheese = cheese
+        self.tomatoes = tomatoes
+        self.cutlets = cutlets
+        self.eggs = eggs
+        self.sauce = sauce
